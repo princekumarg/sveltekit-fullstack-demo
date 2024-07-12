eb8bfa36d4f8e860bc2c50957b26db70f179fed8
prince kumar <agarwal8789@gmail.com>
2024-07-12 10:23:59 +0530

    commit made on bounty

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -68,6 +68,13 @@
             >CloudWallet</a
           >
         </li>
+        <li>
+          <a
+            href="/bounty"
+            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+            >Bounty</a
+          >
+        </li>
       </ul>
     </div>
   </div>
