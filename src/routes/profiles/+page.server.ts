5c2f984900f0c587f2074e5e8fde767117930356
prince kumar <agarwal8789@gmail.com>
2024-07-12 09:27:48 +0530

    cloud-wallet intergation

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -1,4 +1,3 @@
-// import { error } from '@sveltejs/kit';
 import { createPool, sql } from '@vercel/postgres'
 import { POSTGRES_URL } from '$env/static/private'
 
@@ -6,7 +5,7 @@ export async function load() {
   const db = createPool({ connectionString: POSTGRES_URL })
 
   try {
-    const { rows: names } = await db.query('SELECT * FROM names')
+    const { rows: names } = await db.query('SELECT * FROM names order by id')
     return {
       names: names,
     }
@@ -16,7 +15,7 @@ export async function load() {
       )
       // Table is not created yet
       await seed()
-      const { rows: names } = await db.query('SELECT * FROM names')
+      const { rows: names } = await db.query('SELECT * FROM names order by id')
       return {
         users: names
       }
@@ -61,25 +60,51 @@ async function seed() {
   }
 }
 
+async function updateUser(user) {
+    console.log('user', user);
+    const db = createPool({ connectionString: POSTGRES_URL })
+    const client = await db.connect();
+
+    const result = await client.sql`UPDATE names SET name = ${user.name}, email = ${user.email} WHERE id = ${user.id}`
+
+    return {
+      result
+    }
+}
+
 /** @type {import('./$types').Actions} */
 export const actions = {
 	
-//   update: async ({ request }) => {
-//     const data = await request.formData();
-//     const db = createPool({ connectionString: POSTGRES_URL });
-//     const client = await db.connect();
+  update: async ({ request }) => {
+    const req = await request.formData();
 
-//     const id = data.get('id'); // Assuming id is included in the form data
-//     const email = data.get('email');
-//     const name = data.get('name');
+    const id = req.get('id');
+    const name = req.get('name');
+    const email = req.get('email');
 
-//     const updateUser = await client.sql`
-//     UPDATE names
-//     SET email = ${email}, name = ${name}
-//     WHERE id = ${id};`
-	
-//     return { success: true };
-// },
+    const data = {
+      id, name, email
+    }
+
+    let updateRes = {
+      error : false, email : email, name, messsage : ''
+    }
+
+    try {
+      const res = await updateUser(data);
+      console.log('api request ran');
+      console.log(res);
+
+
+    } catch (error) {
+        console.log('api request errored');
+        console.log(error)
+        updateRes.error = true;
+        updateRes.messsage = error.messsage;
+    }finally{
+      return updateRes
+    }
+	},
 
   delete: async ({ request }) => {
     const data = await request.formData();
@@ -92,7 +117,7 @@ export const actions = {
     DELETE FROM names
     WHERE id = ${id};`
 	
-		return { success: true };
+		return { deleted: true };
 	},
 
 	create: async ({request}) => {
@@ -111,6 +136,3 @@ export const actions = {
     return { success: true };
 	}
 };
-
-
-
